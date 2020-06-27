adf404112baa99ea81163e806a2c6006eef34088
Joshua Brook <joshuabrookuk@gmail.com>
2020-06-27 16:04:29 +0100

    RED: Returns 1 when given 1.

---
+++ README.md
@@ -0,0 +1,13 @@
+Fizz Buzz in JS
+
+input | output
+------+-------
+   1  |   1
+   2  |   2
+   3  |   "Fizz"
+   4  |   4
+   5  |   "Buzz"
+   6  |   "Fizz"
+   10 |   "Buzz"
+   15 |   "Fizz Buzz"
+   30 |   "Fizz Buzz"
---
+++ spec/Java_Buzz_Spec.js
@@ -0,0 +1,12 @@
+describe('Java Buzz', function() {
+  it('returns 1 when given 1', function() {
+    expect(JavaBuzz.game(1)).toEqual(1);
+  });
+});
+
+//
+// describe("Test Suite", function() {
+//     it("test spec", function() {
+//         expect( expression ).toEqual(true);
+//     });
+// });
