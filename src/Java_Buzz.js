7aaad7f2d317a1793f1969f542d57d06c0cf680c
Joshua Brook <joshuabrookuk@gmail.com>
2020-06-27 17:17:01 +0100

    GREEN: Returns 'FizzBuzz' when given 15.

--- src/Java_Buzz.js
+++ src/Java_Buzz.js
@@ -3,10 +3,12 @@
 class JavaBuzz {
 
   game(x) {
-    if (x % 3 === 0)
-    return 'Fizz';
+    if (x % 15 === 0)
+    return 'FizzBuzz';
     else if (x % 5 === 0)
     return 'Buzz';
+    else if (x % 3 === 0)
+    return 'Fizz';
     else
     return x
   };
