37be043a3aa28dedb6162c9222c18ab318f96e45
Joshua Brook <joshuabrookuk@gmail.com>
2020-06-27 16:47:38 +0100

    RED: Returns Fizz when given 3.

--- spec/Java_Buzz_Spec.js
+++ spec/Java_Buzz_Spec.js
@@ -11,4 +11,13 @@ describe('Java Buzz', function() {
   it('returns 1 when given 1', function() {
     expect(javaBuzz.game(1)).toEqual(1);
   });
+
+  it('returns 2 when given 2', function() {
+    expect(javaBuzz.game(2)).toEqual(2);
+  });
+
+  it('returns 2 when given 2', function() {
+    expect(javaBuzz.game(3)).toEqual('Fizz');
+  });
+
 });
